d9ad0b5865522dbbb93bd28fe6b7c441ec384091
Allanazarov Nodirbek <pigeonych@gmail.com>
2021-11-01 06:55:08 +0500

    Added focus & tabindex on cards, proptypes checking test

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './charList.scss';
 
@@ -17,6 +18,18 @@ class CharList extends Component {
 	}
 
 	marvelService = new MarvelService();
+
+	itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
 
 	componentDidMount() {
 		this.onRequest();
@@ -58,7 +71,7 @@ class CharList extends Component {
 	}
 
 	renderItems = (arr) => {
-		const items = arr.map (item => {
+		const items = arr.map ((item, i) => {
 			let imgStyle = {"objectFit" : "cover"};
 			if(item.thumbnail === "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg") {
 				imgStyle = {"objectFit" : "unset"};
@@ -66,8 +79,20 @@ class CharList extends Component {
 
 			return (
 				<li className="char__item"
+					tabIndex={0}
+					ref={this.setRef}
 					key={item.id}
-					onClick={() => this.props.onCharSelected(item.id)}>
+					onClick={() => {
+						this.props.onCharSelected(item.id);
+						this.focusOnItem(i);
+						}
+					}
+					onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
 					<img src={item.thumbnail} alt={item.name} style={imgStyle}/>
 					<div className="char__name">{item.name}</div>
 				</li>
@@ -107,6 +132,8 @@ class CharList extends Component {
 	}
 }
 
-
+CharList.propTypes = {
+	onCharSelected: PropTypes.func.isRequired
+}
 
 export default CharList;
